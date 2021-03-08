1dd3dfd6fa79108d728e21d117ed8b6af9f4b60f
OF3787 <mayuanding@qianmi.com>
2021-03-08 17:28:17 +0800

    状态机

---
+++ src/test/java/com/myd/statefsm/StatefsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myd.statefsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatefsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
