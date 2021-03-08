e7e81b6f56b464356aa1bd909b3f875d08aa4f7a
OF3787 <mayuanding@qianmi.com>
2021-03-08 17:27:32 +0800

    状态机

---
+++ src/main/java/com/myd/statefsm/StatefsmApplication.java
@@ -0,0 +1,13 @@
+package com.myd.statefsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatefsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatefsmApplication.class, args);
+	}
+
+}
