e4b1fd389e9ac1847f1001ea191f98e9d1f829b5
OF3787 <mayuanding@qianmi.com>
2021-03-09 14:45:46 +0800

    状态机

---
+++ src/main/java/com/myd/statefsm/state/impl/StateInstallFailed.java
@@ -0,0 +1,27 @@
+package com.myd.statefsm.state.impl;
+
+import com.myd.statefsm.senum.StateEvent;
+import com.myd.statefsm.state.FSMState;
+
+/**
+ * @author <a href="mailto:mayuanding@qianmi.com">OF3787-马元丁</a>
+ * @version 0.1.0
+ * @Date:2021/3/9 10:03
+ * @Description: 安装失败
+ */
+public class StateInstallFailed extends FSMState {
+    @Override
+    protected void doAction(StateEvent event) {
+
+    }
+
+    @Override
+    protected void transState(StateEvent event) {
+
+    }
+
+    @Override
+    public void eventChange(StateEvent event) {
+
+    }
+}
---
+++ src/main/java/com/myd/statefsm/state/impl/StateToInstall.java
@@ -0,0 +1,27 @@
+package com.myd.statefsm.state.impl;
+
+import com.myd.statefsm.senum.StateEvent;
+import com.myd.statefsm.state.FSMState;
+
+/**
+ * @author <a href="mailto:mayuanding@qianmi.com">OF3787-马元丁</a>
+ * @version 0.1.0
+ * @Date:2021/3/9 10:01
+ * @Description: 去下载
+ */
+public class StateToInstall extends FSMState {
+    @Override
+    protected void doAction(StateEvent event) {
+
+    }
+
+    @Override
+    protected void transState(StateEvent event) {
+
+    }
+
+    @Override
+    public void eventChange(StateEvent event) {
+
+    }
+}
